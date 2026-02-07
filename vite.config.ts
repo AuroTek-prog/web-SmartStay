8b3048b5497f00d3d2511961ca723fda972b72d8
AuroTek-prog <aurotek.user@gmail.com>
2026-02-07 16:46:34 +0100

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
+    base: '/Diseño-web-corporativa-SmartStay/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
